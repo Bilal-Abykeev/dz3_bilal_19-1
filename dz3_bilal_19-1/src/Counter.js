34096e7208edaabd65533797df3b76f609c8feb0
Bilal <113285404+Bilal-Abykeev@users.noreply.github.com>
2023-01-06 15:44:04 +0300

    Add files via upload

---
+++ dz3_bilal_19-1/src/Counter.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function Caunter() {
+    
+    const [count,setCount]=useState(1)
+    const plus=()=>{
+        setCount(count+1)
+    }
+
+
+    return (
+      <div className="Caunter">
+        {count}
+        <button className="btn" onClick={plus}>+</button>  
+      </div>
+    );
+  }
+  
+  export default Caunter;
